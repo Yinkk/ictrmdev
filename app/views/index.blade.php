a89c5fd5122cf99b34666cd4271926c6d759a5c5
Yinkk <yeunyong.ka@gmail.com>
2015-05-15 14:51:18 +0700

    add groups and users_groups

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -5,47 +5,47 @@
 <div class="page-header">
 	<h3>Home Page</h3>
 	<?php
-	require_once("assets/nusoap/lib/nusoap.php");
+	// require_once("assets/nusoap/lib/nusoap.php");
 	
-	$user = base64_encode("yeunyong.ka");
-	$pass = base64_encode("");
+	// $user = base64_encode("yeunyong.ka");
+	// $pass = base64_encode("1qazxsw2");
 
-	$method = "Login";
-	$soapaction = "http://tempuri.org/Login";
-	$body = '<Login xmlns="http://tempuri.org/">';
-	$body .= '<username>'.$user.'</username>';
-	$body .= '<password>'.$pass.'</password>';
-	$body .= '<ProductName>decaffair_student</ProductName>';
-	$body .= '</Login>';
+	// $method = "Login";
+	// $soapaction = "http://tempuri.org/Login";
+	// $body = '<Login xmlns="http://tempuri.org/">';
+	// $body .= '<username>'.$user.'</username>';
+	// $body .= '<password>'.$pass.'</password>';
+	// $body .= '<ProductName>decaffair_student</ProductName>';
+	// $body .= '</Login>';
 	
-	$client = new nusoap_client("https://ws.up.ac.th/mobile/AuthenService.asmx?WSDL", true); 
-	//$client = new nusoap_client($wsdl);
-                                                                                        //$client = new nusoap_client($wsdl);
-	$client->decode_utf8 = false;
-	$mysoapmsg = $client->serializeEnvelope($body,'',array(),'document','literal');	
-	$response = @$client->send($mysoapmsg,$soapaction);
-	$result = $response['LoginResult'];
-	$SID = $result;
-	//print($SID);
-	//$this->session->set_userdata('SID', $SID);
+	// $client = new nusoap_client("https://ws.up.ac.th/mobile/AuthenService.asmx?WSDL", true); 
+	// //$client = new nusoap_client($wsdl);
+ //                                                                                        //$client = new nusoap_client($wsdl);
+	// $client->decode_utf8 = false;
+	// $mysoapmsg = $client->serializeEnvelope($body,'',array(),'document','literal');	
+	// $response = @$client->send($mysoapmsg,$soapaction);
+	// $result = $response['LoginResult'];
+	// $SID = $result;
+	// //print($SID);
+	// //$this->session->set_userdata('SID', $SID);
 
-	//$wsdl = "https://ws.up.ac.th/mobile/StaffService.asmx?WSDL";
-	$method = "GetStaffInfo";
-	$soapaction = "http://tempuri.org/GetStaffInfo";
-	$body = '<GetStaffInfo   xmlns="http://tempuri.org/">';
-	$body .= '<sessionID>'.$SID.'</sessionID>';
-	$body .= '</GetStaffInfo >';
+	// //$wsdl = "https://ws.up.ac.th/mobile/StaffService.asmx?WSDL";
+	// $method = "GetStaffInfo";
+	// $soapaction = "http://tempuri.org/GetStaffInfo";
+	// $body = '<GetStaffInfo   xmlns="http://tempuri.org/">';
+	// $body .= '<sessionID>'.$SID.'</sessionID>';
+	// $body .= '</GetStaffInfo >';
 
-	$client = new nusoap_client("https://ws.up.ac.th/mobile/StaffService.asmx?WSDL", true); 
-	$client->decode_utf8 = false;
-	$mysoapmsg=$client->serializeEnvelope($body,'',array(),'document','literal');	
-	$response=@$client->send($mysoapmsg,$soapaction);
-	$result = $response['GetStaffInfoResult'];
-	var_dump($result);
+	// $client = new nusoap_client("https://ws.up.ac.th/mobile/StaffService.asmx?WSDL", true); 
+	// $client->decode_utf8 = false;
+	// $mysoapmsg=$client->serializeEnvelope($body,'',array(),'document','literal');	
+	// $response=@$client->send($mysoapmsg,$soapaction);
+	// $result = $response['GetStaffInfoResult'];
+	// var_dump($result);
 
 
-	echo $result['Title'];
-	echo $result['FirstName_TH'];
+	// echo $result['Title'];
+	// echo $result['FirstName_TH'];
 	
 
 	?>
